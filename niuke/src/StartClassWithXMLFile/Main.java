6b0531676ca5840108c479e3a5c06279d0dead44
杨启晅 <yangqixuan@cpic.com.cn>
2020-05-09 17:10:05 +0800

    实现功能：主方法添加注释 实现程度：100% 实现日期：2020-05-09

--- niuke/src/StartClassWithXMLFile/Main.java
+++ niuke/src/StartClassWithXMLFile/Main.java
@@ -2,6 +2,12 @@ package StartClassWithXMLFile;
 
 import StartClassWithXMLFile.LoadClass;
 public class Main {
+    /**
+     * 实现通过解析xml文件中的class路径来加载类
+     * @param args
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     */
     public static void main(String[] args) throws InstantiationException, IllegalAccessException {
         LoadClass.printFruit("a","b", "Apple");
     }
