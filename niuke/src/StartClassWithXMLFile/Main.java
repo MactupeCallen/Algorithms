9316c3a638248ce4a9a8e0c6714d5a051e39d2ce
Mactupe <Mactupe@163.com>
2020-05-09 18:22:44 +0800

    实现功能：添加注释
    实现程度：100%
    实现日期：2020-05-08

--- niuke/src/StartClassWithXMLFile/Main.java
+++ niuke/src/StartClassWithXMLFile/Main.java
@@ -3,7 +3,7 @@ package StartClassWithXMLFile;
 import StartClassWithXMLFile.LoadClass;
 public class Main {
     /**
-     * 实现通过解析xml文件中的class路径来加载类
+     * 实现通过解析xml文件中的class路径来加载类  
      * @param args
      * @throws InstantiationException
      * @throws IllegalAccessException
